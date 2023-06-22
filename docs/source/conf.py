a7ed3967e6d96937004bbedddfd5d58a7ff10a7f
flaport <floris.laporte@gmail.com>
2023-06-22 12:09:08 -0700

    Bump version: 0.6.6 → 0.6.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ for k in src:
 project = "meow"
 copyright = "2022, Apache2"
 author = "Floris Laporte"
-release = "0.6.6"
+release = "0.6.7"
 extensions = [
     "myst_nb",
     "matplotlib.sphinxext.plot_directive",
