dc5eb913fb01e2195db502b82b9f3e2cb9cb414d
flaport <floris.laporte@gmail.com>
2024-06-20 10:45:18 +0200

    Bump version: 0.10.0 → 0.11.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ for k in src:
 project = "meow"
 copyright = "2022, Apache2"
 author = "Floris Laporte"
-release = "0.10.0"
+release = "0.11.0"
 extensions = [
     "myst_nb",
     "matplotlib.sphinxext.plot_directive",
--- meow/__init__.py
+++ meow/__init__.py
@@ -1,7 +1,7 @@
 """ MEOW: Modeling of Eigenmodes and Overlaps in Waveguides """
 
 __author__ = "Floris Laporte"
-__version__ = "0.10.0"
+__version__ = "0.11.0"
 
 from .array import Dim as Dim
 from .array import DType as DType
