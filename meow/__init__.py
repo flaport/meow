53531cc90bbfed80364fff18c254f4f44a84e2d1
flaport <floris.laporte@gmail.com>
2024-08-06 07:56:51 +0200

    Bump version: 0.11.1 → 0.11.2

--- meow/__init__.py
+++ meow/__init__.py
@@ -1,7 +1,7 @@
 """ MEOW: Modeling of Eigenmodes and Overlaps in Waveguides """
 
 __author__ = "Floris Laporte"
-__version__ = "0.11.1"
+__version__ = "0.11.2"
 
 from .array import Dim as Dim
 from .array import DType as DType
